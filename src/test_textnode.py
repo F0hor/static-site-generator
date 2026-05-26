f63421d42d14496a999e94516070344d8faec07a
F0hor <f.hor@proton.me>
2026-05-26 15:39:34 +0200

    init

---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_differecne(self):
+        node = TextNode("This is a text", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_type_difference(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_url_difference(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, url='www.boot.dev')
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
