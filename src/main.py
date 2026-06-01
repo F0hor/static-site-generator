9865c33e0d9d0c97077ce3fc8b697dd7866d5ec6
F0hor <f.hor@proton.me>
2026-06-01 15:35:45 +0200

    main to generate html using template

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import os
 import shutil
 
+from block_converter import markdown_to_blocks, markdown_to_html_node
+from textnode import BlockType
+
 
 def copy_content_to_folder(source: str, destination: str):
     for name in os.listdir(source):
@@ -15,8 +18,48 @@ def copy_content_to_folder(source: str, destination: str):
         else:
             dst = os.path.join(destination, name)
             os.mkdir(dst)
-            print(f'created: dst')
+            print(f'created: {dst}')
             copy_content_to_folder(path, os.path.join(destination, name))
+
+
+def extrect_title(markdown: str) -> str:
+    blocks = markdown_to_blocks(markdown)
+
+    for b in blocks:
+        if b.startswith('# '):
+            return b[2:]
+
+    raise Exception('Missing title in form of h1 header')
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, 'r') as f:
+        markdown = f.read()
+
+    with open(template_path, 'r') as f:
+        template = f.read()
+
+    title = extrect_title(markdown)
+    html_body = markdown_to_html_node(markdown).to_html()
+
+    html = template.replace('{{ Title }}', title).replace('{{ Content }}', html_body)
+
+    with open(dest_path, "w") as f:
+        f.write(html)
+
+
+def generate_content(source: str, template: str, dest: str):
+    for name in os.listdir(source):
+        source_path = os.path.join(source, name)
+        dest_path = os.path.join(dest, name)
+
+        if os.path.isfile(source_path):
+            dest_path = dest_path.replace('.md', '.html')
+            generate_page(source_path, template, dest_path)
+        else:
+            generate_content(source_path, template, dest_path)
 
 
 def main():
@@ -24,9 +67,12 @@ def main():
     os.mkdir(PATH_TO_PUBLIC)
 
     copy_content_to_folder(PATH_TO_STATIC, PATH_TO_PUBLIC)
+    generate_content(PATH_TO_CONTENT, PATH_TO_TEMPLATE, PATH_TO_PUBLIC)
     
 
 PATH_TO_PUBLIC = 'public/'
 PATH_TO_STATIC = 'static/'
+PATH_TO_CONTENT = 'content/'
+PATH_TO_TEMPLATE = 'template.html'
 main()
 
